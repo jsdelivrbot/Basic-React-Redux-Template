7e74ade6cceb08791e32a7e5882cbbb8a74e3545
Kevin Kim <Kevin.Kim@itg.com>
2018-07-27 16:14:11 -0700

    update rootReducer file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,3 @@
-import { combineReducers } from 'redux';
-
-//app specific reducers
-import PlayersReducers from './reducer_players';
-import ActivePlayerReducers from './reducer_activePlayer';
-
-//map the state to the appropriate reducers
-const rootReducer = combineReducers({
-  players: PlayersReducers,
-  activePlayer: ActivePlayerReducers,
-});
-
-export default rootReducer;
+export default function(state) {
+	return state;
+}
